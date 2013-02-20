e6e8f0fbb520052c1afc1ef5b1d4b69cdff76eec
Dev Gurjar <d25inferno@gmail.com>
2013-02-19 21:59:58 -0500

    Added module visual support for javascript Added zoom and drag interactions with mouse. Cleaned commented code

--- styler.js
+++ styler.js
@@ -6,48 +6,48 @@ var commits = [];
 var comm = [];
 var scale_factor = 1;
 
-var min_opacity = .15;
+var min_opacity = .3;
 var max_opacity = 1
 
-var max_height 	= window.innerWidth*2;
-var max_width 	= window.innerWidth*2;
 
 //TODO: Include this to display modularity
 //Vertical position of all functions
 var functions = [];
 
+var lastX, lastY;
+
 
 
 function init(){
-	char_width = 2.5;
-	char_height = 5;
+    char_width = 2.5;
+    char_height = 5;
 
     canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d');
 
-    opacity.match 	= .25;
-    opacity.comment = .25;
-    opacity.neg 	= .25;
-    opacity.base 	= .25;
+    opacity.match 	= .3;
+    opacity.comment = .3;
+    opacity.neg 	= .3;
+    opacity.base 	= .3;
+
+	trackTransforms(context);
+
 
     /* Resize the canvas to fill browser window dynamically */
     window.addEventListener('resize', resizeCanvas, false);
-   
     resizeCanvas();
 
-	
-	$("#plus").click(function(){
-		scale_factor*=1.5;
-		reDrawAll();
-		//context.scale(2,2);
-	});
 
-	$("#minus").click(function(){
-		scale_factor/=1.5;
-		reDrawAll();
-		//context.scale(.5,.5);
-	});
-	
+    $("#plus").click(function(){
+        scale_factor*=1.5;
+        reDrawAll();
+    });
+
+    $("#minus").click(function(){
+        scale_factor/=1.5;
+        reDrawAll();
+    });
+
 	$("#sel_com").click(function(){
 		opacity.comment = (opacity.comment == 1) ? min_opacity:max_opacity;
 		reDrawAll();
@@ -62,30 +62,126 @@ function init(){
 		opacity.neg = (opacity.neg == 1) ? min_opacity:max_opacity;
 		reDrawAll();
 	});
+
+	lastX = canvas.width/2;
+	lastY = canvas.height/2;
+	var dragStart,dragged;
+	canvas.addEventListener('mousedown',function(evt){
+		document.body.style.mozUserSelect = document.body.style.webkitUserSelect = document.body.style.userSelect = 'none';
+		lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+		lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+		dragStart = context.transformedPoint(lastX,lastY);
+		dragged = false;
+	},false);
+	canvas.addEventListener('mousemove',function(evt){
+		lastX = evt.offsetX || (evt.pageX - canvas.offsetLeft);
+		lastY = evt.offsetY || (evt.pageY - canvas.offsetTop);
+		dragged = true;
+		if (dragStart){
+			var pt = context.transformedPoint(lastX,lastY);
+			context.translate(pt.x-dragStart.x,pt.y-dragStart.y);
+			reDrawAll();
+		}
+	},false);
+	canvas.addEventListener('mouseup',function(evt){
+		dragStart = null;
+	},false);
+
+	var scaleFactor = 1.1;
+	var zoom = function(clicks){
+		var pt = context.transformedPoint(lastX,lastY);
+		context.translate(pt.x,pt.y);
+		var factor = Math.pow(scaleFactor,clicks);
+		context.scale(factor,factor);
+		context.translate(-pt.x,-pt.y);
+		reDrawAll();
+	}
+
+	var handleScroll = function(evt){
+		var delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
+		if (delta) zoom(delta);
+		return evt.preventDefault() && false;
+	};
+	canvas.addEventListener('DOMMouseScroll',handleScroll,false);
+	canvas.addEventListener('mousewheel',handleScroll,false);
 }
 
 /* Resizes the canvas width and height based on the context */
 function resizeCanvas() {
-    canvas.width = max_width;
-    canvas.height = max_height;
-    reDrawAll(); 
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    lastX = canvas.width/2;
+    lastY = canvas.height/2;
+    context.clearRect(0,0,canvas.width,canvas.height);
+    reDrawAll();
+}
+
+function trackTransforms(){
+	var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
+	var xform = svg.createSVGMatrix();
+	context.getTransform = function(){ return xform; };
+	
+	var savedTransforms = [];
+	var save = context.save;
+	context.save = function(){
+		savedTransforms.push(xform.translate(0,0));
+		return save.call(context);
+	};
+	var restore = context.restore;
+	context.restore = function(){
+		xform = savedTransforms.pop();
+		return restore.call(context);
+	};
+
+	var scale = context.scale;
+	context.scale = function(sx,sy){
+		xform = xform.scaleNonUniform(sx,sy);
+		return scale.call(context,sx,sy);
+	};
+	var rotate = context.rotate;
+	context.rotate = function(radians){
+		xform = xform.rotate(radians*180/Math.PI);
+		return rotate.call(context,radians);
+	};
+	var translate = context.translate;
+	context.translate = function(dx,dy){
+		xform = xform.translate(dx,dy);
+		return translate.call(context,dx,dy);
+	};
+	var transform = context.transform;
+	context.transform = function(a,b,c,d,e,f){
+		var m2 = svg.createSVGMatrix();
+		m2.a=a; m2.b=b; m2.c=c; m2.d=d; m2.e=e; m2.f=f;
+		xform = xform.multiply(m2);
+		return transform.call(context,a,b,c,d,e,f);
+	};
+	var setTransform = context.setTransform;
+	context.setTransform = function(a,b,c,d,e,f){
+		xform.a = a;
+		xform.b = b;
+		xform.c = c;
+		xform.d = d;
+		xform.e = e;
+		xform.f = f;
+		return setTransform.call(context,a,b,c,d,e,f);
+	};
+	var pt  = svg.createSVGPoint();
+	context.transformedPoint = function(x,y){
+		pt.x=x; pt.y=y;
+		return pt.matrixTransform(xform.inverse());
+	}
 }
 
 /* Redraws the entire canvas using the current parameters */
 function reDrawAll(){
-	context.clearRect(0, 0, canvas.width, canvas.height);
-
-	/* Replace the max height if necessary */
-	var code_width = 2*80*char_width*commits.length*scale_factor;
-	if(code_width > max_width)
-		max_width = code_width;
+	/* Clears the canvas */
+	var p1 = context.transformedPoint(0,0);
+	var p2 = context.transformedPoint(canvas.width,canvas.height);
+	context.clearRect(p1.x,p1.y,p2.x-p1.x,p2.y-p1.y);
 
 	for(var i = 0; i<commits.length; i++){
 		drawCode(commits[i].code, commits[i].lang, i*scale_factor*80*char_width);
 	}
-
-	//TODO: This is not working, figure out why
-	//resizeCanvas();
 }
 
 /* Fetches new data from GitHub */
@@ -95,17 +191,27 @@ function update(){
 	comm = [];
 
 	/* Clear the context */
-	context.clearRect(0, 0, canvas.width, canvas.height);
+	var p1 = context.transformedPoint(0,0);
+	var p2 = context.transformedPoint(canvas.width,canvas.height);
+	context.clearRect(p1.x,p1.y,p2.x-p1.x,p2.y-p1.y);
 
 	/* Parse the url to get necessary information */
 	url = $("#url").val();
 	var urlDat = parseURL(url);
 
 	/* Bad url case */
-	if(urlDat.success !== true)
+	if(urlDat.success !== true){
+		alert("Sorry, something is wrong with this URL. Try again.");
 		return false;
+	}
 
 	$.getJSON("https://api.github.com/repos/" + urlDat.user + "/" + urlDat.repos + "/commits?callback=?", function(a) {
+		/* Unable to fetch gitHub data */
+		if(a.meta.status !== 200){
+			alert("Unable to get data from Github, it is possible you have exceeded 60 requests. Try again in 1 hour.");
+			return;
+		}
+
   		//Populate the commits
   		for(var i = 0; i<a.data.length; i++){
   			comm[a.data.length-1-i] = a.data[i];
@@ -113,6 +219,7 @@ function update(){
 
   		for(var i = 0; i<a.data.length; i++){
   			$.getJSON("https://api.github.com/repos/" + urlDat.user + "/" + urlDat.repos + "/contents/" + urlDat.fp + "?ref=" + comm[i].sha + "&callback=?", function(b){
+				console.log(b);
 				var code = Gh3.Base64.decode(b.data.content);
 				var sha = b.data.url.split("ref=")[1];
 				var index = getCommitIndex(sha, comm);
@@ -134,8 +241,8 @@ function update(){
 
 /* Get the index of the commmit given it's sha */
 function getCommitIndex(sha){
-	for(var i = 0; i<comm.length; i++){
-		if(comm[i].sha === sha)
+    for(var i = 0; i<comm.length; i++){
+        if(comm[i].sha === sha)
 			return i;
 	}
 	return -1;
@@ -152,15 +259,13 @@ function drawCode(code, language, h_index){
 
 	highlighter = new (findBrush(language));
 
-	//Clear Vertical position of all functions
+	//Clear vertical position of all functions
 	functions = [];
 
-	/* Replace the max height if necessary */
-	var code_height = 2*lines.length*char_height*scale_factor;
-	if(code_height > max_height)
-		max_height = code_height;
-
 	for(var i=0; i<lines.length; i++){
+		if(lines[i].indexOf("function ") !== -1){
+			functions.push(i*char_height*scale_factor)
+		}
 		var matches = SyntaxHighlighter.Highlighter.prototype.findMatches(highlighter.regexList, lines[i]);
 		drawBase(lines[i], i, h_index, opacity.base);
 		drawNegative(lines[i], i, h_index, opacity.neg);
@@ -170,9 +275,6 @@ function drawCode(code, language, h_index){
 	}
 
 	drawFunctions(h_index);
-
-	/* Commit elements light up when hovered over */
-	//commitDiv.hover(function(){dimSubElements($(this), 1); $(this).css("z-index", 9999);}, function(){dimSubElements($(this), .25); $(this).css("z-index", $(this).position().left);});
 }
 
 function drawFunctions(h_index){
@@ -254,6 +356,7 @@ function drawMatch(match, v_index, h_index, opacity_comm, opacity_match){
 
 	context.fill();
 }
+
 
 /* Given the parent div and the new opacity, sets this for all of the sub elements */
 function dimSubElements(parent, opacity){
